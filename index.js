04c22d2c4a67ecde52fa26fce573038408d99f10
Max <jmlong.dev@gmail.com>
2022-06-08 19:07:03 -0400

    added chalk top color the terminal outputs. needs bug fixing to address readdir

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
+// import chalk from "chalk";
+//Method #2
+// const lstat = util.promisify(fs.lstat);
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method #3
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, process.cwd(), async (err, filenames) => {
   if (err) {
-    // throw new Error(err);
     console.log(err);
   }
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
+  });
+  const allStats = await Promise.all(statPromises);
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
 
-  // BAD CODE EXAMPLE
-  for (let filename of filenames) {
-    const allStats = [];
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(filename, stats.isFile());
-    });
+    if (stats.isFile()) console.log(filenames[index]);
+    else console.log(chalk.bgGreen(filenames[index]));
   }
 });
+
+// Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
