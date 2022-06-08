251b7d4feb633ff0f0bd344f53ac6a25d12af727
Max <jmlong.dev@gmail.com>
2022-06-07 22:56:59 -0400

    sampling different implementations if lstat, starting with the bad

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
-fs.readdir(".", (err, filenames) => {
-  // EITHER
-  // err === an error object, which means something went wrong
-  // OR
-  // err === null, which means everything is ok
-
+fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
-    // error handling code here
     // throw new Error(err);
     console.log(err);
   }
 
-  console.log(filenames);
+  // BAD CODE EXAMPLE
+  for (let filename of filenames) {
+    const allStats = [];
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(filename, stats.isFile());
+    });
+  }
 });
